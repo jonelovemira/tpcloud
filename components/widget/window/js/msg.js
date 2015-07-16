309ba5c7ddf2911abb043872a0cf8441dd9455ad
monitor <monitor@localhost.localdomain>
2015-07-16 05:51:54 +0000

    a) style.css和字体文件 m) 修改msg类中html元素的类名以及样式

--- components/widget/window/js/msg.js
+++ components/widget/window/js/msg.js
@@ -2,25 +2,23 @@
     var Msg = {
         ID : 0,
         mainTemplate :  '<div >' + 
-                        '<div class="seven-windows-holder"></div>' +
-                        '<div class="seven-windows-position">' + 
-                            '<div class="seven-windows-contain">' +
-                                '<span class="seven-windows-head">' +
-                                    '<span class="seven-windows-head-title"></span>' +
-                                    '<span class="seven-windows-head-close"></span>' +
-                                '</span>' +
-                                '<span class="seven-windows-body">' +
-                                    '<span class="seven-windows-body-contain">' +
-                                        '<span class="seven-windows-body-contain-font"></span>' +
-                                    '</span>' +
-                                '</span>' +
-                                '<span class="seven-windows-body-foot">' +
-                                    '<span class="seven-windows-body-foot-contain"></span>' +
-                                '</span>' +
+                        '<div class="window-msg-bg"></div>' +
+                        '<div class="window-msg-holder">' + 
+                            '<div class="window-msg-contain">' +
+                                '<div class="window-msg-head">' +
+                                    '<span class="window-msg-head-title"></span>' +
+                                '</div>' +
+                                '<div class="window-msg-body">' +
+                                    '<div class="window-msg-body-content">' +
+                                    '</div>' +
+                                '</div>' +
+                                '<div class="window-msg-body-foot">' +
+                                '</div>' +
                             '</div>' +
                         '</div>' +
                     '</div>',
-        btnTemplate : '<input class="seven-windows-button seven-windows-alert-button" value="OK" type="button" />',
+        closeIconTemplate : '<span class="window-msg-head-close"></span>',
+        btnTemplate : '<input class="window-msg-btn" value="OK" type="button" />',
         close : function(msg){
             var context = msg.parent();
             $('body').unbind('keydown', Msg.hotClose);
@@ -66,36 +64,44 @@
 
         //render id, title, info, button and size
         msg.attr("id", id);
-        $('.seven-windows-head-title', msg).text(_options.title);
-        $('.seven-windows-body-contain-font', msg).text(_options.info); 
+        $('.window-msg-head-title', msg).text(_options.title);
+        $('.window-msg-body-content', msg).text(_options.info); 
         
         switch(_options.type)
         {
             case "alert":
-                $('.seven-windows-body-foot-contain', msg).append(Msg.btnTemplate);
-                $('.seven-windows-alert-button:first', msg).attr("value", _options.btnOk);
-                $('.seven-windows-alert-button:first', msg).attr("for", "ok");
+                $('.window-msg-body-foot', msg).append(Msg.btnTemplate);
+                $('.window-msg-btn:first', msg).attr("value", _options.btnOk);
+                $('.window-msg-btn:first', msg).attr("for", "ok");
             break;
 
             case "confirm":
-                $('.seven-windows-body-foot-contain', msg).append(Msg.btnTemplate);
-                $('.seven-windows-body-foot-contain', msg).append(Msg.btnTemplate);
-                $('.seven-windows-alert-button:first', msg).attr("value", _options.btnCancel);
-                $('.seven-windows-alert-button:first', msg).attr("for", "cancel");
-                $('.seven-windows-alert-button:last', msg).attr("value", _options.btnConfirm);
-                $('.seven-windows-alert-button:last', msg).attr("for", "confirm");
+                $('.window-msg-body-foot', msg).append(Msg.btnTemplate);
+                $('.window-msg-body-foot', msg).append(Msg.btnTemplate);
+                $('.window-msg-btn:first', msg).attr("value", _options.btnCancel);
+                $('.window-msg-btn:first', msg).attr("for", "cancel");
+                $('.window-msg-btn:last', msg).attr("value", _options.btnConfirm);
+                $('.window-msg-btn:last', msg).attr("for", "confirm");
+            break;
+
+            case "info":
             break;
 
             default:
-                $('.seven-windows-body-foot-contain', msg).append(Msg.btnTemplate);
-                $('.seven-windows-alert-button:first', msg).attr("value", _options.btnOk);
+                $('.window-msg-body-foot', msg).append(Msg.btnTemplate);
+                $('.window-msg-btn:first', msg).attr("value", _options.btnOk);
+                $('.window-msg-btn:first', msg).attr("for", "ok");
             break;
         }
 
-        $('.seven-windows-contain', msg).css({
+        $('.window-msg-contain', msg).css({
             "width": _options.width,
             "height": _options.height
         });
+
+        if (_options.closeIcon) {
+            $('.window-msg-head', msg).append(Msg.closeIconTemplate);
+        };
 
         _options.beforeInit();
 
@@ -110,7 +116,7 @@
 
         msg.option = _options;
 
-        // add object functions. so we can dynamic call this functions.
+        // add object functions. so we can dynamic call these functions.
         msg.close = function(){
             Msg.close(msg);
         };
@@ -128,18 +134,20 @@
             Msg.ok(msg);
         }
 
-        $('.seven-windows-alert-button', msg).on('click', function(e){
+        $('.window-msg-btn', msg).on('click', function(e){
             e.preventDefault();
             var type = $(this).attr("for");
             Msg[type](msg); 
         });
 
-        $('.seven-windows-head-close', msg).on('click', function(e){
+        $('.window-msg-head-close', msg).on('click', function(e){
             e.preventDefault();
             Msg.close(msg);
         });
 
-        $('body').bind('keydown', {msg: msg}, Msg.hotClose);
+        if (msg.option.hotKey) {
+            $('body').bind('keydown', {msg: msg}, Msg.hotClose);
+        };
 
         return msg;
     };
@@ -155,6 +163,8 @@
         btnOk       :       "OK",
         width       :       400,
         height      :       260,
+        hotKey      :       true,
+        closeIcon   :       true,
         cancel      :       function() {},
         ok          :       function() {},
         confirm     :       function() {},
