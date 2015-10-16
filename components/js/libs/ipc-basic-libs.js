3010ce91d1a239429bc84e881fc835292a447e09
monitor <jone@god.com>
2015-10-16 01:02:56 +0000

    m) 浏览器判断支持edge

--- components/js/libs/ipc-basic-libs.js
+++ components/js/libs/ipc-basic-libs.js
@@ -1,139 +1,141 @@
 (function ($) {
-	
-	$.BrowserTypeVersion = (function(){
-		var ua = navigator.userAgent, tem;
-		var M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-		if(/trident/i.test(M[1])){
-			tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
-			return 'IE '+(tem[1] || '');
-		}
-		if(M[1]=== 'Chrome'){			
-			tem= ua.match(/\bOPR\/(\d+)/);
-			if(tem!= null) return 'Opera '+tem[1];
-		}
-		M = M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-		if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
-		return M.join(' ');
-	})();
+    
+    $.BrowserTypeVersion = (function(){
+        var ua= navigator.userAgent, tem, 
+        M= ua.match(/(opera|chrome|safari|firefox|msie|edge|trident(?=\/))\/?\s*(\d+)/i) || [];
+        if(/trident/i.test(M[1])){
+            tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+            return 'IE '+(tem[1] || '');
+        }
+        if(M[1]=== 'Chrome'){
+            tem= ua.match(/\bOPR\/(\d+)/);
+            if(tem!= null) return 'Opera '+tem[1];
+            tem = ua.match(/Edge\/(\d+)/);
+            if(tem!= null) return 'Edge '+tem[1];
+        }
+        M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+        if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+        return M.join(' ');
+    })();
 
-	function ieXDomainAjax(options){	
-		if (window.XDomainRequest == undefined) {
-			throw "no XDomainRequest in current browser";
-			return;
-		}
-		var xdr = new XDomainRequest();
-		if (xdr == undefined) {
-			throw "can not create XDomainRequest instance";
-			return;
-		};
+    function ieXDomainAjax(options){    
+        if (window.XDomainRequest == undefined) {
+            throw "no XDomainRequest in current browser";
+            return;
+        }
+        var xdr = new XDomainRequest();
+        if (xdr == undefined) {
+            throw "can not create XDomainRequest instance";
+            return;
+        };
 
-		var newOptions = $.extend(true, {}, $.xAjax.ieXDomainAjaxDefaults, options);
+        var newOptions = $.extend(true, {}, $.xAjax.ieXDomainAjaxDefaults, options);
 
-		if (newOptions.url == undefined) {
-			throw "no url in ieXDomainAjax";
-			return;
-		};
+        if (newOptions.url == undefined) {
+            throw "no url in ieXDomainAjax";
+            return;
+        };
 
-		xdr.onerror = function(){
-			(options.error || function(xdr){})(xdr);
-		}
+        xdr.onerror = function(){
+            (options.error || function(xdr){})(xdr);
+        }
 
-		xdr.ontimeout = function(){
-			(options.error || function(xdr){})(xdr);
-		}
+        xdr.ontimeout = function(){
+            (options.error || function(xdr){})(xdr);
+        }
 
-		xdr.onload = function(){
-			var response = $.parseJSON(xdr.responseText);
-			(options.success || function(resp){})(response);
-		}
+        xdr.onload = function(){
+            var response = $.parseJSON(xdr.responseText);
+            (options.success || function(resp){})(response);
+        }
 
-		xdr.onprogress = function(){
-			(options.onprogress || function(xdr){})(xdr);
-		}
+        xdr.onprogress = function(){
+            (options.onprogress || function(xdr){})(xdr);
+        }
 
-		xdr.timeout = newOptions.timeout;
-		xdr.open(newOptions.type, newOptions.url);
-		xdr.send(newOptions.data);
-	};
+        xdr.timeout = newOptions.timeout;
+        xdr.open(newOptions.type, newOptions.url);
+        xdr.send(newOptions.data);
+    };
 
-	function normalAjax (options) {
-		var newOptions = $.extend(true, {}, $.xAjax.normalAjaxDefaults, options);
-	
-		if (newOptions.url == undefined) {
-			throw "options.url in undefined in normalAjax";
-			return;
-		}
+    function normalAjax (options) {
+        var newOptions = $.extend(true, {}, $.xAjax.normalAjaxDefaults, options);
+    
+        if (newOptions.url == undefined) {
+            throw "options.url in undefined in normalAjax";
+            return;
+        }
 
-		newOptions.beforeSend = function(){
-			(options.beforeSend || function(){})();
-		}
-		newOptions.complete = function(xhr){
-			var ajaxContext = this;
-			$.proxy((options.complete || function(xhr){}), ajaxContext)(xhr);
-		}
-		newOptions.error = function(xhr){
-			(options.error || function(xhr){})(xhr);
-		}
-		newOptions.success = function(response){
-			var ajaxContext = this;
-			$.proxy((options.success || function(resp){}), ajaxContext)(response);
-		}
+        newOptions.beforeSend = function(){
+            (options.beforeSend || function(){})();
+        }
+        newOptions.complete = function(xhr){
+            var ajaxContext = this;
+            $.proxy((options.complete || function(xhr){}), ajaxContext)(xhr);
+        }
+        newOptions.error = function(xhr){
+            (options.error || function(xhr){})(xhr);
+        }
+        newOptions.success = function(response){
+            var ajaxContext = this;
+            $.proxy((options.success || function(resp){}), ajaxContext)(response);
+        }
 
-		var ajaxObj = $.ajax(newOptions);
-		return ajaxObj;
-	};
+        var ajaxObj = $.ajax(newOptions);
+        return ajaxObj;
+    };
 
-	var browserAjaxMap = {
-		"MSIE 8": ieXDomainAjax,
-		"MSIE 9": ieXDomainAjax,
-		"MSIE 10": ieXDomainAjax
-	};
+    var browserAjaxMap = {
+        "MSIE 8": ieXDomainAjax,
+        "MSIE 9": ieXDomainAjax,
+        "MSIE 10": ieXDomainAjax
+    };
 
-	$.xAjax = function(options, xDomain)
-	{
-		var ajaxFunction = xDomainAjaxMap[xDomain] || normalAjax;
-		var ajaxObj = ajaxFunction(options);
-		return ajaxObj;
-	};
+    $.xAjax = function(options, xDomain)
+    {
+        var ajaxFunction = xDomainAjaxMap[xDomain] || normalAjax;
+        var ajaxObj = ajaxFunction(options);
+        return ajaxObj;
+    };
 
-	$.xAjax.normalAjaxDefaults = {
-		type : "post",
-		data : {},
-		dataType : "json",				
-		cache : false,
-		contentType : "application/json;charset=utf-8",
-		timeout : 60000,
-		async : true,
-		global : false
-	};
+    $.xAjax.normalAjaxDefaults = {
+        type : "post",
+        data : {},
+        dataType : "json",              
+        cache : false,
+        contentType : "application/json;charset=utf-8",
+        timeout : 60000,
+        async : true,
+        global : false
+    };
 
-	$.xAjax.ieXDomainAjaxDefaults = {
-		timeout : 60000,
-		type : "post",
-		data : {}
-	};
+    $.xAjax.ieXDomainAjaxDefaults = {
+        timeout : 60000,
+        type : "post",
+        data : {}
+    };
 
-	$.xAjax.defaults = {
-		xType : "xDomain"
-	};
+    $.xAjax.defaults = {
+        xType : "xDomain"
+    };
 
-	var xDomainStr = $.xAjax.defaults.xType;
-	var xDomainAjaxMap = {};
-	xDomainAjaxMap[xDomainStr] = browserAjaxMap[$.BrowserTypeVersion] || normalAjax;
+    var xDomainStr = $.xAjax.defaults.xType;
+    var xDomainAjaxMap = {};
+    xDomainAjaxMap[xDomainStr] = browserAjaxMap[$.BrowserTypeVersion] || normalAjax;
 
 
-	$.OneOrNoneTrim = function(str, mode) {
-		if (null == str) {
-			return;
-		};
-		var result;
-		var str = str;
-		result = str.replace(/(^\s+)|(\s+$)/g, ""); 
-		if (mode == "all") {
-			result = result.replace(/\s/g, "");
-		} else if (mode == "one") { 
-			result = result.replace(/\s+/g, " ");
-		}
-		return result;
-	}
+    $.OneOrNoneTrim = function(str, mode) {
+        if (null == str) {
+            return;
+        };
+        var result;
+        var str = str;
+        result = str.replace(/(^\s+)|(\s+$)/g, ""); 
+        if (mode == "all") {
+            result = result.replace(/\s/g, "");
+        } else if (mode == "one") { 
+            result = result.replace(/\s+/g, " ");
+        }
+        return result;
+    }
 })(jQuery);
