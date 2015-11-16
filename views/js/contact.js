293e44f742b39b61dc5844f4485f4cd438a0b823
monitor <jone@god.com>
2015-11-16 11:34:02 +0000

    m) contact页面重构完成

--- views/js/contact.js
+++ views/js/contact.js
@@ -6,5 +6,27 @@ $(function(){
 
     $.ipc.inheritPrototype(SoftwareController, $.ipc.BaseController);
 
-    
+    SoftwareController.prototype.getUpdateInfos = function() {
+        var currentController = this;
+        var inputCallbacks = {
+            0: function() {
+                this.view.feedModelSelect();
+            }
+        };
+
+        this.model.getUpdateInfos(inputCallbacks);
+    };
+
+    function SoftwareView () {
+        this.model = null;
+    };
+
+    SoftwareView.prototype.feedModelSelect = function () {
+        var addedOptions = {};
+        for (var i = 0; i < this.model.products.length; i++) {
+            addedOptions[this.model.products[i].name] = {};
+        };
+        $("#model_select").Select({addedOptions: addedOptions});
+    };
+
 });
